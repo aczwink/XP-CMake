0f3e00b3a86a5d40cb07b50b96e2225e68069c21
Amir Czwink <amir130@hotmail.de>
2018-04-02 14:32:57 +0200

    -added todos

--- install.bat
+++ install.bat
@@ -1,2 +1,3 @@
 @echo off
 reg add "HKCU\Software\Kitware\CMake\Packages\XP-CMake" /v Path /t REG_SZ /d %~dp0
+pause
--- modules/Compiler.cmake
+++ modules/Compiler.cmake
@@ -1,6 +1,9 @@
 macro(Compiler_OptForHost)
 	if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU") #gcc
 		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=native")
+	elseif(MSVC)
+		#TODO: msvc does not have such an option
+		#need to query cpu features and set them
 	else()
 		message(FATAL_ERROR "NOT IMPLEMENTED")
 	endif()
